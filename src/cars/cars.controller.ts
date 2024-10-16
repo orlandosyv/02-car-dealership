f39a195f42c2c188888fa0f955fa5bedd64defe8
Orlando YV <sergioyv@gmail.com>
2024-10-15 23:19:20 -0500

    oy - inyeccion de dependencias

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,19 +1,22 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
 
-    private cars = ['Toyota', 'Honda', 'Jeep'];
+    constructor(private readonly carsService: CarsService) {
+
+    }
 
     @Get()
     getAllCars() {
-        return this.cars;
+        return this.carsService.findAll();
     }
 
     @Get(':id')
     getCardById(@Param('id') id: string) {
         const index = parseInt(id, 10);  // Convert id to a number
         console.log({ id, index  });
-        return this.cars[index ];
+        return this.carsService.findCarById(index);
     }
 }
