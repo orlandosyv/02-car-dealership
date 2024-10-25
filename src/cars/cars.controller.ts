ca3d466705dec3bbeac0e52cd3a5ccd8670e0939
Orlando YV <sergioyv@gmail.com>
2024-10-24 22:28:40 -0500

    oy - fix of patch

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -28,7 +28,8 @@ export class CarsController {
     }
 
     @Patch(':id')
-    updateCar(@Body() body: any) {
+    updateCar(@Param('id', ParseIntPipe) id: number,
+        @Body() body: any) {
         return body;
     }
 
